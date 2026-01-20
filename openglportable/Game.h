5502f6fe40aa045e5bcd27c5ab6c9661a90d52f3
Intisar22 <b.m.intisartanjim555@gmail.com>
2026-01-21 00:37:51 +0600

    2nd Commit

--- openglportable/Game.h
+++ openglportable/Game.h
@@ -403,4 +403,125 @@ public:
                 meteorX[i] = rand() % (SCREEN_WIDTH - 50);
             }
         }
+        if(showFuelTank) {
+            fuelTankY -= 2;
+            if(fuelTankY < 0) showFuelTank = false;
+
+            if (player.x + 80 > fuelTankX && player.x < fuelTankX + 50 &&
+                player.y + 80 > fuelTankY && player.y < fuelTankY + 50) {
+                player.fuel = 100;
+                showFuelTank = false;
+            }
+        }
+
+        fuelTickTimer++;
+        if (fuelTickTimer >= 30) {
+            player.fuel--;
+            fuelTickTimer = 0;
+        }
+
+        fuelTankSpawnTimer++;
+        if (!showFuelTank && fuelTankSpawnTimer >= 40 * 60) {
+            showFuelTank = true;
+            fuelTankX = rand() % (SCREEN_WIDTH - 50);
+            fuelTankY = SCREEN_HEIGHT;
+            fuelTankSpawnTimer = 0;
+        }
+
+        if (player.fuel <= 0) {
+            timeSinceFuelEmpty++;
+            if (timeSinceFuelEmpty >= 5 * 60) {
+                player.health -= 10;
+                timeSinceFuelEmpty = 0;
+            }
+        } else {
+            timeSinceFuelEmpty = 0;
+        }
+
+        if (player.score > highScore) {
+            highScore = player.score;
+            strcpy(highScorer, player.name);
+            saveHighScore(player.score, player.name);
+        }
+
+        if (player.health <= 0) {
+             FILE *f = fopen("leaderboard.txt", "a");
+             if (f) {
+                 fprintf(f, "%d %s\n", player.score, player.name);
+                 fclose(f);
+             }
+             if (player.score > highScore) {
+                 highScore = player.score;
+                 strcpy(highScorer, player.name);
+                 saveHighScore(player.score, player.name);
+             }
+             currentState = GAME_OVER;
+             isPaused = false;
+             PlaySound("missionpass.wav", NULL, SND_ASYNC);
+             applyMusicVolume();
+        }
+    }
+
+    void draw() {
+        iClear();
+
+        if (currentState == LOADING) {
+            iShowImage(0, 0, "loadingscreen.png");
+            return;
+        }
+
+        if (currentState == MENU) {
+            iShowImage(0, 0, "menu.png");
+
+            if (hoveredButtonIndex != -1) {
+                int expand = 6;
+                int bx = buttonX - expand;
+                int by = buttonYStart - hoveredButtonIndex * buttonSpacing - expand;
+                int bw = buttonWidth + 2 * expand;
+                int bh = buttonHeight + 2 * expand;
+
+                iSetColor(0, 255, 250);
+                iSetLineWidth(3);
+                iRectangle(bx, by, bw, bh);
+            }
+
+            if (showMusicSettings) {
+                iShowImage(0, 0, "music.png");
+                iSetColor(0, 255, 255);
+                iText(500, 50, "Press 'h' to get back to the menu page", GLUT_BITMAP_HELVETICA_18);
+                return;
+            }
+            if (showHelp) {
+                iShowImage(0, 0, "help.png");
+            }
+            if (showCredit) {
+                 int x = (SCREEN_WIDTH - 800) / 2;
+                 int y = (SCREEN_HEIGHT - 600) / 2;
+                 iSetColor(255, 255, 255);
+                 iFilledRectangle(x - 10, y - 10, 820, 620);
+                 iShowImage(x, y, "credit(1).png");
+            }
+            return;
+        }
+
+        if (currentState == NAME_ENTRY) {
+            iShowImage(0, 0, "name.png");
+            iSetColor(255, 0, 0);
+            iText(375, 340, player.name, GLUT_BITMAP_HELVETICA_18);
+            return;
+        }
+
+        if (currentState == LEADERBOARD) {
+            iShowImage(0, 0, "leaderboard_bg.png");
+            iSetColor(255, 223, 0);
+            iText(SCREEN_WIDTH / 2 - 60, SCREEN_HEIGHT - 100, "TOP 10 PLAYERS", GLUT_BITMAP_TIMES_ROMAN_24);
+
+            for (int i = 0; i < totalScores; i++) {
+                char entry[100];
+                sprintf(entry, "%d. %s - %d", i + 1, topScores[i].name, topScores[i].score);
+                iText(SCREEN_WIDTH / 2 - 150, SCREEN_HEIGHT - 150 - i * 30, entry, GLUT_BITMAP_HELVETICA_18);
+            }
+            iText(SCREEN_WIDTH / 2 - 100, 50, "Press 'B' to go back", GLUT_BITMAP_HELVETICA_12);
+            return;
+        }
 
